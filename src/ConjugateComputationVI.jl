4cda5f91b2a843fea9d894fc8801b0d8aa1d137c
WT <wt0881@my.bristol.ac.uk>
2021-03-29 21:43:53 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/ConjugateComputationVI.jl
@@ -0,0 +1,5 @@
+module ConjugateComputationVI
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ConjugateComputationVI
+using Test
+
+@testset "ConjugateComputationVI.jl" begin
+    # Write your tests here.
+end
