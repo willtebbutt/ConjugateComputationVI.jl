16af2aa44955f5d2d86bfff8522bbe8e7321fa67
WT <wt0881@my.bristol.ac.uk>
2021-04-09 18:43:02 +0100

    Add tests for update_approx_posterior

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,27 @@
 using AbstractGPs
 using ConjugateComputationVI
 using KernelFunctions
+using Random
 using TemporalGPs
 using Test
 
 using ConjugateComputationVI:
     approx_posterior,
     canonical_pseudo_obs,
-    natural_pseudo_obs
+    natural_pseudo_obs,
+    update_approx_posterior
+
+function generate_synthetic_problem(rng::AbstractRNG)
+    f = GP(Matern52Kernel())
+    x = collect(range(-5.0, 5.0; length=31))
+    σ² = rand(rng, 31) .+ 1e-2
+    y = rand(rng, f(x, σ²))
+    return f, x, σ², y
+end
 
 @testset "ConjugateComputationVI.jl" begin
     @testset "parameter_conversion" begin
-        y = randn(10)
-        σ² = randn(10)
+        _, _, σ², y = generate_synthetic_problem(MersenneTwister(123456))
         η1, η2 = natural_pseudo_obs(y, σ²)
         y_recovered, σ²_recovered = canonical_pseudo_obs(η1, η2)
 
@@ -21,12 +30,7 @@ using ConjugateComputationVI:
         @test σ² ≈ σ²_recovered
     end
     @testset "approx_posterior" begin
-
-        # Generate a GP and sample from it.
-        f = GP(SEKernel())
-        x = collect(range(-5.0, 5.0; length=31))
-        σ² = rand(31) .+ 1e-2
-        y = rand(f(x, σ²))
+        f, x, σ², y = generate_synthetic_problem(MersenneTwister(123456))
 
         # Compute natural pseudo-observations and approximate posterior.
         η1, η2 = natural_pseudo_obs(y, σ²)
@@ -42,5 +46,30 @@ using ConjugateComputationVI:
         ms_approx = marginals(f_post_approx(x_test))
         @test mean.(ms_exact) ≈ mean.(ms_approx)
         @test std.(ms_exact) ≈ std.(ms_approx)
+    end
+    @testset "update_approx_posterior" begin
+        f, x, σ², y = generate_synthetic_problem(MersenneTwister(123456))
+
+        @testset "optimal parameters don't move" begin
+
+            # Run a step of the update procedure.
+            η1, η2 = natural_pseudo_obs(y, σ²)
+            η1_new, η2_new = update_approx_posterior(f, x, η1, η2, ∇r, 1.0)
+
+            # Verify that the new parameters are equal to the old parameters.
+            @test η1 ≈ η1_new
+            @test η2 ≈ η2_new
+        end
+
+        # Verify the optimal approximate posterior parameters are found after only a single
+        # iteration of the algorithm in the Gaussian case.
+        @testset "optimal in 1 step" begin
+            η1, η2 = natural_pseudo_obs(y .+ randn(length(y)), σ² .+ rand(length(y)))
+            η1_opt, η2_opt = update_approx_posterior(f, x, η1, η2, ∇r, 1.0)
+
+            y_opt, σ²_opt = canonical_pseudo_obs(η1_opt, η2_opt)
+            @test y ≈ y_opt
+            @test σ² ≈ σ²_opt
+        end
     end
 end
