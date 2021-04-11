e3fc0353be88a5d6cf1d1b0da36db8246c22da34
WT <wt0881@my.bristol.ac.uk>
2021-04-11 17:45:44 +0100

    Add binary classification example

--- examples/classification_model.jl
+++ examples/classification_model.jl
@@ -1,8 +1,19 @@
 using AbstractGPs
 using ConjugateComputationVI
+using Distributions
+using Optim
+using ParameterHandling
+using Plots
+using Random
 using RDatasets
+using StatsFuns
+using Zygote
 
-using ConjugateComputationVI: approx_posterior, batch_quadrature
+using ConjugateComputationVI:
+    approx_posterior,
+    batch_quadrature,
+    elbo,
+    optimise_approx_posterior
 
 # Get the (infamous) Iris dataset, and only keep two labels -- we'll just do binary
 # classification for now.
@@ -11,6 +22,13 @@ iris = filter(
     dataset("datasets", "iris"),
 )
 
+setosa = filter(row -> row.Species == "setosa", iris)
+virginica = filter(row -> row.Species == "virginica", iris)
+
+scatter(setosa.SepalLength, setosa.SepalWidth; label="Setosa")
+scatter!(virginica.SepalLength, virginica.SepalWidth; label="Virginica")
+
+
 # Check that there are only two features.
 @assert length(unique(iris.Species)) == 2
 
@@ -18,38 +36,139 @@ iris = filter(
 y = map(x -> x == "setosa" ? 1 : 0, iris.Species)
 
 # Construct input matrix from feature columns.
-x = RowVecs(hcat(iris.SepalLength, iris.SepalWidth, iris.PetalLength, iris.PetalWidth))
+X = hcat(iris.SepalLength, iris.SepalWidth)
+x = ColVecs(collect(X'))
 
-# Specify the GP (we'll not do hyperparameter optimisation in the first-pass).
-# We'll use a unit length scale. Can always tweak by hand if necessary.
-f = GP(SEKernel())
-σ² = 1e-3
+# Split into train and test sets.
+train_indices = randperm(length(y))[1:50]
+test_indices = setdiff(eachindex(y), train_indices)
+x_tr = x[train_indices]
+x_te = x[test_indices]
+y_tr = y[train_indices]
+y_te = y[test_indices]
+
+θ_init = (
+    scale=fixed(1.0),
+    stretch=positive.(ones(2)),
+)
+
+θ_init_flat, unflatten = ParameterHandling.flatten(θ_init)
+
+build_gp(θ::AbstractVector{<:Real}) = build_gp(ParameterHandling.value(unflatten(θ)))
+build_gp(θ::NamedTuple) = GP(θ.scale * AbstractGPs.transform(SEKernel(), θ.stretch))
 
 # Specify reconstruction term.
 function make_integrand(y)
     return f -> logpdf(Bernoulli(logistic(f)), y)
 end
-integrands = map(make_integrand, y)
+integrands = map(make_integrand, y_tr)
 
 # Specify the reconstruction term.
-r(m̃, σ̃²) = sum(batch_quadrature(integrands, m̃, sqrt.(σ̃²), 25))
+r(m̃, σ̃²) = sum(batch_quadrature(integrands, m̃, sqrt.(σ̃²), 10))
 
-# Run VI to convergence. This should happen in only a few iterations.
-η1_0 = randn(length(y))
-η2_0 = - rand(length(y))
-ρ = 0.9
-η1_opt, η2_opt, iteration, delta_norm = optimise_approx_posterior(
-    f, x, η1_0, η2_0, r, ρ; max_iterations=5,
+function objective(θ::AbstractVector{<:Real})
+
+    # Regularise the parameters a bit. Don't want them to be too large.
+    regularisation = 0.0 * sum(abs2, θ)
+
+    # Unflatten θ and build the model at the current hyperparameters.
+    f = build_gp(θ)
+
+    # Optimise the approximate posterior. Drop the gradient because we're differentiating
+    # through the optimum.
+    η1_opt, η2_opt = Zygote.ignore() do
+        η1_0 = randn(length(x_tr))
+        η2_0 = -rand(length(x_tr)) .- 1
+        η1, η2, iters, delta = optimise_approx_posterior(
+            f, x_tr, η1_0, η2_0, r, 1 - 1e-6; tol=1e-8, max_iterations=1_000,
+        )
+        println((iters, delta))
+        return η1, η2
+    end
+
+    # Compute the negation of the elbo.
+    return -elbo(f, x_tr, η1_opt, η2_opt, r) + regularisation
+end
+
+objective(θ_init_flat)
+Zygote.gradient(objective, θ_init_flat)
+
+# Optimise the ELBO using Optim.
+training_results = Optim.optimize(
+    objective,
+    θ -> only(Zygote.gradient(objective, θ)),
+    θ_init_flat,
+    BFGS(
+        alphaguess = Optim.LineSearches.InitialStatic(scaled=true),
+        linesearch = Optim.LineSearches.BackTracking(),
+    ),
+    Optim.Options(
+        show_trace = true,
+        iterations=25,
+        f_calls_limit=50,
+    );
+    inplace=false,
 )
-@show iteration, delta_norm
 
-# Make some predictions using the same batch quadrature routine.
-ms = marginals(approx_posterior(f, x, η1_opt, η2_opt)(x))
-conditionals = fill(f -> pdf(Bernoulli(logistic(f)), 1), length(ms))
-posterior_mean_predictions = batch_quadrature(conditionals, mean.(ms), std.(ms), 25)
+f = build_gp(training_results.minimizer)
+η1_0 = randn(length(x_tr))
+η2_0 = -rand(length(x_tr)) .- 1
+η1, η2, iters, delta = optimise_approx_posterior(
+    f, x_tr, η1_0, η2_0, r, 1 - 1e-6; tol=1e-12,
+)
 
-# Make MAP predictions and compute %age correctly classified.
-MAP_predictions = map(x -> x > 0.5 ? 1 : 0, posterior_mean_predictions)
-100 * count(MAP_predictions .== y) / length(y)
+function latent_marginals(x::AbstractVector)
+    return marginals(approx_posterior(f, x_tr, η1, η2)(x))
+end
 
-# As expected, it is extremely easy to fit the training data in this task.
+function post_mean_prediction(x::AbstractVector)
+    ms = latent_marginals(x)
+    conditionals = fill(f -> pdf(Bernoulli(logistic(f)), 1), length(ms))
+    posterior_mean_predictions = batch_quadrature(conditionals, mean.(ms), std.(ms), 25)
+    return posterior_mean_predictions
+end
+
+# Compute predictions using the optimal hyperparameters.
+function MAP_predict(x::AbstractVector)
+    posterior_mean_predictions = post_mean_prediction(x)
+    MAP_predictions = map(x -> x > 0.5 ? 1 : 0, posterior_mean_predictions)
+    return MAP_predictions
+end
+
+# Compute training set accuracy as a %age.
+100 * mean(MAP_predict(x_tr) .== y_tr)
+
+# Compute test set accuracy as a %age.
+100 * mean(MAP_predict(x_te) .== y_te)
+
+# Compute prediction surface.
+x1_range = range(minimum(X[:, 1]) - 3, maximum(X[:, 1]) + 3; length=100);
+x2_range = range(minimum(X[:, 2]) - 3, maximum(X[:, 2]) + 3; length=100);
+x_pr_vecs = [[x1, x2] for x1 in x1_range, x2 in x2_range];
+X_pr = reduce(hcat, x_pr_vecs);
+x_pr = ColVecs(X_pr);
+
+
+# Plot classification.
+MAP_pr = MAP_predict(x_pr);
+p1 = heatmap(x1_range, x2_range, reshape(MAP_pr, length(x2_range), length(x1_range))'; label="classifier");
+scatter!(p1, setosa.SepalLength, setosa.SepalWidth; label="");
+scatter!(p1, virginica.SepalLength, virginica.SepalWidth; label="");
+
+# Plot posterior mean prob.
+post_mean_pr = post_mean_prediction(x_pr);
+p2 = heatmap(x1_range, x2_range, reshape(post_mean_pr, length(x2_range), length(x1_range))'; label="prob");
+scatter!(p2, setosa.SepalLength, setosa.SepalWidth; label="");
+scatter!(p2, virginica.SepalLength, virginica.SepalWidth; label="");
+
+# Plot posterior marginals.
+ms = latent_marginals(x_pr);
+p3 = heatmap(x1_range, x2_range, reshape(mean.(ms), length(x2_range), length(x1_range))'; label="latent mean");
+scatter!(p3, setosa.SepalLength, setosa.SepalWidth; label="");
+scatter!(p3, virginica.SepalLength, virginica.SepalWidth; label="");
+
+p4 = heatmap(x1_range, x2_range, reshape(std.(ms), length(x2_range), length(x1_range))'; label="latent std");
+scatter!(p4, setosa.SepalLength, setosa.SepalWidth; label="");
+scatter!(p4, virginica.SepalLength, virginica.SepalWidth; label="");
+
+plot(p1, p2, p3, p4; layout=(2, 2))
